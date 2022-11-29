ad1e303509478a2c14bdebdfd8cd5055c3bb1069
dingjiachengcn <dingjiachengcn@outlook.com>
2022-11-28 21:25:44 -0500

    Demo6

---
+++ src/Demo6.java
@@ -0,0 +1,27 @@
+public class Demo6 public class demo6 {
+    public static void main(String[] args) {
+        //学会关系运算符
+        int a = 10;
+        int b = 10;
+        a == b;
+        boolean rs = a == b;
+        System.out.println(rs);
+
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a < b);
+        System.out.println(a <= b);
+
+        int i = 10;
+        int j = 5;
+        System.out.println(i == j);
+        System.out.println(i != j);
+        System.out.println(i > j);
+        System.out.println(i >= j);
+        System.out.println(i < j);
+        System.out.println(i <= j);
+    }
+}
+
